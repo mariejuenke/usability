303d3d74387608405c11bbc92bc5d287649ca746
mariejuenke <m.juenke@gmx.de>
2026-05-25 16:28:04 +0200

    remove unused files

--- server.py
+++
@@ -1,8 +0,0 @@
-import os
-import subprocess
-
-port = os.environ.get("PORT", "8501")
-
-cmd = f"streamlit run app.py --server.port {port} --server.address 0.0.0.0"
-
-subprocess.call(cmd, shell=True)
